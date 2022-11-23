3628982b3a16fb6f51a144a9222a44dfe385eaec
pkumar <piyush.kumar@logmein.com>
2022-11-23 13:22:04 +0530

    initial commit

---
+++ src/main/java/com/example/protobuf/Test.java
@@ -0,0 +1,15 @@
+/*
+ *  Copyright (c) 2022 GoTo
+ *  All Rights Reserved Worldwide.
+ *
+ *  THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO GOTO
+ *  AND CONSTITUTES A VALUABLE TRADE SECRET.
+ */
+package com.example.protobuf;
+
+/**
+ * @author Piyush Kumar.
+ * @since 23/11/22.
+ */
+public class Test {
+}
---
+++ src/test/java/com/example/protobuf/TestTest.java
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (c) 2022 GoTo
+ *  All Rights Reserved Worldwide.
+ *
+ *  THIS PROGRAM IS CONFIDENTIAL AND PROPRIETARY TO GOTO
+ *  AND CONSTITUTES A VALUABLE TRADE SECRET.
+ */
+package com.example.protobuf;
+
+/**
+ * @author Piyush Kumar.
+ * @since 23/11/22.
+ */
+class TestTest {
+
+}
